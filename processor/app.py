1336d8a857dbda18abfc7c72b41ff26780789284
Hugo Luccarini <hluccarini@genetec.com>
2023-07-30 00:25:43 +0200

    accept images

--- processor/app.py
+++ processor/app.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from cv2 import aruco
+
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "hello world"
+@app.route("/process", methods=["POST"])
+def process_image():
+    file = request.files['image']
+    img = Image.open(file.stream)
+
+    # TODO: check if contains marker, if not return HTTP 404
+
+    # TODO: get audio description
+
+    return jsonify({'msg': 'success', 'size': [img.width, img.height]})

52a15021251a52ee6d051c4328fd17caf96cb4ec
Hugo Luccarini <hluccarini@genetec.com>
2023-07-30 01:50:43 +0200

    add test app

--- processor/app.py
+++ processor/app.py
@@ -1,15 +1,17 @@
 from flask import Flask, request, jsonify
+
 from cv2 import aruco
 
 app = Flask(__name__)
 
 @app.route("/process", methods=["POST"])
 def process_image():
-    file = request.files['image']
-    img = Image.open(file.stream)
+    file = request.data
 
     # TODO: check if contains marker, if not return HTTP 404
 
     # TODO: get audio description
 
-    return jsonify({'msg': 'success', 'size': [img.width, img.height]})
+    response = jsonify({'audio-description': "placeholder"})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response

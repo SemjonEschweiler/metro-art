394de8e9dcc977af50b3b3370faed283015819fe
Hugo Luccarini <hluccarini@genetec.com>
2023-07-30 02:36:48 +0200

    handle image

--- processor/app.py
+++ processor/app.py
@@ -1,12 +1,22 @@
-from flask import Flask, request, jsonify
+import base64
+import cv2
+import flask
+import io
+import PIL
+import numpy
+from PIL import Image
 
-from cv2 import aruco
+app = flask.Flask(__name__)
 
-app = Flask(__name__)
+def base64ToNp(imageAsBase64):
+    imgdata = base64.b64decode(imageAsBase64)
+    image = Image.open(io.BytesIO(imgdata))
+
+    return cv2.cvtColor(numpy.array(image), cv2.COLOR_BGR2RGB)
 
 @app.route("/process", methods=["POST"])
 def process_image():
-    file = request.data
+    openCvImage = base64ToNp(flask.request.data)
 
     # TODO: check if contains marker, if not return HTTP 404
 
@@ -14,6 +24,6 @@ def process_image():
 
     # TODO: get audio description from id + corners
 
-    response = jsonify({'audio-description': "placeholder"})
+    response = flask.jsonify({'audio-description': "placeholder"})
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response

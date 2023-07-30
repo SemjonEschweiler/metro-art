13796994c181975e6a1ffcf51ae97e14ae876a78
Chrge <35903713+Chrge@users.noreply.github.com>
2023-07-30 04:48:01 +0200

    app.py edges returned, none if no edges

---
+++ processor/output_ID.py
@@ -0,0 +1,41 @@
+import cv2
+from cv2 import aruco
+
+def detect_aruco_ids_in_image(image_path):
+    # Load the predefined dictionary
+    aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_250)
+
+    # Initialize the detector parameters using default values
+    parameters = aruco.DetectorParameters()
+
+    # Load the image
+    image = cv2.imread(image_path)
+
+    # Check if image is loaded
+    if image is None:
+        print(f'Failed to load image from path: {image_path}')
+        return
+
+    # Convert the image to grayscale
+    grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Create the Aruco detector
+    detector = aruco.ArucoDetector(aruco_dict, parameters)
+
+    # Detect the markers in the image
+    corners, ids, rejectedImgPoints = detector.detectMarkers(grayscale)
+
+    # If at least one marker detected
+    if len(corners) > 0:
+        print(f'Detected IDs in {image_path}:', ids)
+        # draw a square around the markers
+        cv2.aruco.drawDetectedMarkers(image, corners, ids)
+        
+        # Display the resulting frame
+        cv2.imshow('frame', image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    else:
+        # No Markers in Image
+        return None
+
